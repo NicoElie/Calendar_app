7b477c58bb44b362a5d24fbe7830d7c28880f218
eliemas <masanka2@illinois.edu>
2020-03-16 00:46:15 -0500

    modified Information.py, _Calendar.py, and test.py.

--- Information.py
+++ Information.py
@@ -51,11 +51,7 @@ class Student:
         self.password = password
         self.major = major
         self.semester = semester
-        # must come from the Course class
-        # so student_courses store object
         self.student_courses = []  # store all the course taken by the students for the current semester
-        # must come from the activity class
-        # so student_activities store object
         self.student_activities = []  # store students activities such as jobs, extracurricular...
 
     def add_course(self, course_name):  # add a course to student_courses
@@ -63,7 +59,6 @@ class Student:
 
     def remove_course(self, course_name):
         self.student_courses.remove(course_name)
-        return None
 
     def get_student_courses(self):  # return student_courses
         return self.student_courses
@@ -100,6 +95,9 @@ class Course:
         self.course_name = str(course_name)
         self.course_code = str(course_code)
         self.section = str(section)
+        self.exams = []  # will store Exam object
+        self.quiz = []  # will store Quiz object, only one because quizzes are repetitive event
+        self.homework = []  # will store Homework object, only one because homework is a repetitive event
 
     def get_course_name(self):
         return self.course_name
@@ -145,42 +143,61 @@ class Course:
             # [9,0,50], [0, 2, 4])
         return [hour_start, minute_start, duration], days_list
 
-    @staticmethod
-    def homework_deadline(has_homework, deadline):
-        # if the course has homework, return the deadline day
-        if has_homework:
-            return deadline
+    def add_exam(self, start_time_and_duration, date):  # add exams in order please
+        self.exams.append(Exam(self.course_code, start_time_and_duration, date))  # this will create a EXam Object
 
-    @staticmethod
-    def lab_deadline(has_lab, deadline):
-        # if the course has lab, return the deadline day
-        if has_lab:
-            return deadline
+    def get_exam(self, which_one):  # which_one will be like 1 for exam 1, and so forth, final will be last element
+        return self.exams[which_one - 1]
 
-    @staticmethod
-    def quiz_deadline(has_quiz, deadline):
-        # if the course has quiz, return the deadline day
-        if has_quiz:
-            return deadline
+    def remove_exam(self, which_one):  # which_one will be like 1 for exam 1, and so forth, final will be last element
+        self.exams.pop(which_one - 1)  # remove based on index
 
-    @staticmethod
-    def exam_date(has_exam):
-        # if the course has exams, return the respective deadline
-        deadline: List[List[int]] = []  # list of lists [[month,day, duration]]
-        if has_exam:
-            number_of_exams = int(input("How many exams: "))
-            i = 1
-            while i <= number_of_exams:
-                date = []  # [month, day, duration]
-                day = int(input("what day: "))  # should be something like 23
-                month = int(input("What month: "))  # month in number please
-                duration = int(input("duration: "))  # duration in minute
-                date.append(month)  # append to date
-                date.append(day)  # append to date
-                date.append(duration)
-                deadline.append(date)  # append to deadline
-                i += 1
-        return deadline
+    def get_exam_name(self, which_one):  # get the exam name
+        return self.get_exam(which_one).get_exam_name()
+
+    def get_exam_start_time_and_duration(self, which_one):  # get the exam start time and duration
+        return self.get_exam(which_one).get_exam_start_time_and_duration()
+
+    def get_exam_date(self, which_one):
+        return self.get_exam(which_one).get_exam_date()
+
+    def add_quiz(self, quiz_date, start_time_and_duration, quiz_type):
+        self.quiz.append(Quiz(self.course_code, quiz_date, start_time_and_duration, quiz_type))
+
+    def remove_quiz(self):
+        self.quiz.pop(0)
+
+    def get_quiz(self):
+        return self.quiz[0]
+
+    def get_quiz_name(self):
+        return self.get_quiz().get_quiz_name()
+
+    def get_quiz_date(self):
+        return self.get_quiz().get_quiz_date()
+
+    def get_quiz_start_time_and_duration(self):
+        return self.get_quiz().get_quiz_start_time_and_duration()
+
+    def get_quiz_type(self):
+        return self.get_quiz().get_quiz_type()
+
+    def add_homework(self, due_date):
+        self.homework.append(Homework(self.course_code, due_date))
+
+    def remove_homework(self):
+        self.homework.pop(0)
+
+    def get_homework(self):
+        return self.homework[0]
+
+    def get_homework_name(self):
+        return self.get_homework().get_homework_name()
+
+    def get_homework_due_date(self):
+        return self.get_homework().get_homework_due_date()
+
+
 # -------------------------------------------------------------------------------------------------------------------#
 
 
@@ -198,21 +215,17 @@ we will have:
 
 class Activity:
 
-    def __init__(self, activity_name, start_time, duration, day):
+    def __init__(self, activity_name, start_time_and_duration, day):
         self.activity_name = activity_name
-        self.start_time = start_time  # start_time is a list such as [9, 0] 9 o'clock
-        self.duration = duration  # duration of the activity in minute
+        self.start_time_and_duration = start_time_and_duration  # start_time is a list such as [9, 0, 50] 9 o'clock
         self.day = day  # day when the activity happen (for repetitive activity)
         self.month = None  # None is the default value
 
     def get_activity_name(self):
         return self.activity_name
 
-    def get_start_time(self):
-        return self.start_time
-
-    def get_duration(self):
-        return self.duration
+    def get_start_time_and_duration(self):
+        return self.get_start_time_and_duration()
 
     def get_day(self):
         return self.day
@@ -231,28 +244,24 @@ class Activity:
 class Exam:
 This class will represents the skeleton of an exam
 we will have:
-- exam name ( exam 1, exam 2,...) # I don't know if it's necessary
--start time of the exam #will be a list such like [9, 0] to say that it starts at 9 o'clock
-- duration # will be given in minutes
+- exam name ( exam for ECE110,...)
+-start time_and_duration of the exam will be a list such like [9, 0, 50] to say that it starts at 9 o'clock with
+duration 50 min
 - date of the exam # will be a list such as [month, day] # we will assume it is the same year
 """
 
 
 class Exam:
-    def __init__(self, name, start_time, duration, date):
-        self.name = name
-        self.start_time = start_time  # list such as [9, 0] 9 o'clock
-        self.duration = duration  # given in minute
+    def __init__(self, course_code, start_time_and_duration, date):
+        self.name = f"Exam for {course_code}"
+        self.start_time_and_duration = start_time_and_duration  # list such as [9, 0, 50] 9 o'clock 50 min for duration
         self.date = date  # list such as [month, day]
 
     def get_exam_name(self):
         return self.name
 
-    def get_exam_start_time(self):
-        return self.start_time
-
-    def get_exam_duration(self):
-        return self.duration
+    def get_exam_start_time_and_duration(self):
+        return self.start_time_and_duration
 
     def get_exam_date(self):
         return self.date
@@ -263,17 +272,17 @@ class Exam:
 class Homework
 This class will represents the skeleton of a Homework
 we will have:
-- homework name ( homework 1, homework 2,...) # I don't know if it's necessary
-- due_date will be a list like [month,day] # we will not focus on the time at which the homework is due
+- homework name ( homework for ECE110,...)
+- due_date will be a list of week day like monday etc . we will not focus on the time at which the homework is due
 because we want the students to be done before that date
 
 """
 
 
 class Homework:
-    def __init__(self, name, due_date):
-        self.name = name
-        self.due_date = due_date  # [month, day]
+    def __init__(self, course_code, due_date):
+        self.name = f"Homework for {course_code}"
+        self.due_date = due_date  # a list containing the week day when HW is due like [0,4] for Monday and Friday
 
     def get_homework_name(self):
         return self.name
@@ -287,24 +296,19 @@ class Homework:
 class Quiz
 This class will represents the skeleton of a Homework
 we will have:
-- quiz name ( homework 1, homework 2,...) # I don't know if it's necessary
-- quiz_date will be a list like [month,day] 
-- start_time will be a list like [9, 0] which means it starts at 9 o'clock
+- quiz name ( homework for ECE110,...)
+- quiz_date will be a list like [week day]  a list of week day 0 for monday and so forth
+- start_time_and_duration will be a list like [9, 0, 50] which means it starts at 9 o'clock, duration 50 min
 - quiz_type , mainly to know if it is a CBTF quiz or a in class quiz like during discussion session
-- if it is a CBTF quiz, we should ask for duration
-- if it is a discussion type quiz, the discussion duartion will be used instead
-because we want the students to be done before that date
-
 """
 
 
 class Quiz:
-    def __init__(self, name, quiz_date, start_time, quiz_type, duration):
-        self.name = name
-        self.quiz_date = quiz_date  # [month, day]
-        self.start_time = start_time  # [9,0] 9 o'clock
+    def __init__(self, course_code, quiz_date, start_time_and_duration, quiz_type):
+        self.name = f"Quiz for {course_code}"
+        self.quiz_date = quiz_date  # [week day] [0] means monday
+        self.start_time_and_duration = start_time_and_duration  # [9,0, 50] 9 o'clock and 50 min for duration
         self.quiz_type = quiz_type  # CBTF or Discussion
-        self.duration = duration  # given in minutes
 
     def get_quiz_name(self):
         return self.name
@@ -312,30 +316,9 @@ class Quiz:
     def get_quiz_date(self):
         return self.quiz_date
 
-    def get_quiz_start_time(self):
-        return self.start_time
+    def get_quiz_start_time_and_duration(self):
+        return self.start_time_and_duration
 
     def get_quiz_type(self):
         return self.quiz_type
-
-    def get_quiz_duration(self):
-        return self.duration
-
-
-# create a student
-Elie = Student("Elie", "Masanka", "masanka2@illinois.edu", "23", "Statistics", "spring")
-# create a course for Elie
-course1 = Course("Intro to Elec", "ECE110", "AL1")
-# add the course into Elie
-Elie.add_course(course1.get_course_code())
-# check if it is added correctly
-print(Elie.get_student_courses())
-print(course1.get_time_and_day())
-# course_d = Elie.get_student_courses()[0]
-# ECE110 = Course("ECE110", "ECE110", "AL1")
-# ECE110.add_course("ECE110")
-# print(Elie.get_student_courses())
-# ECE110_homework = Homework("ECE110_HW", [3, 15])
-# d = ECE110.homework_deadline(has_homework=True, deadline=ECE110_homework)
-# Elie.add_course(ECE110)
-# print(Elie.get_student_courses()[0])
+# --------------------------------------------------------------------------------------------------------------------#
